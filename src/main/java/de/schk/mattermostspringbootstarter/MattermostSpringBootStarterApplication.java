8a26117105f7983c5617c60731a67dfaa87d1d36
Basler182 <showdialog@web.de>
2025-02-04 22:04:18 +0100

    init project spring mattermost

---
+++ src/main/java/de/schk/mattermostspringbootstarter/MattermostSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package de.schk.mattermostspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MattermostSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MattermostSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/schk/mattermostspringbootstarter/MattermostSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.schk.mattermostspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MattermostSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
